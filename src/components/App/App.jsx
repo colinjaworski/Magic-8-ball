85b1ec800de9aead28bb72aa3187b0a8d2af39d7
colin jaworski <colinrjaworski.84@gmail.com>
2022-05-06 10:24:24 -0500

    appending random eight ball messages to DOM

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,8 +3,32 @@ import './App.css';
 
 function App() {
 
-  function eightBall() {
-    console.log('sup bitch?')
+  const [eightBall, setEightBall] = useState('');
+
+  function eightBallFunction() {
+
+    let randomNumber = Math.floor(Math.random() * 8);
+
+
+    switch (randomNumber) {
+      case 7: setEightBall('It is certain')
+        break;
+      case 6: setEightBall('It is decidedly so')
+        break;
+      case 5: setEightBall('Reply hazy try again')
+        break;
+      case 4: setEightBall('Cannot predict now')
+        break;
+      case 3: setEightBall('Do not count on it')
+        break;
+      case 2: setEightBall('My sources say no')
+        break;
+      case 1: setEightBall('Outlook not so good')
+        break;
+      case 0: setEightBall('Signs point to yes')
+    }
+    console.log(eightBall);
+
   }
 
   return (
@@ -15,7 +39,8 @@ function App() {
       <div className="App">
         <div>
           <input type="text" />
-          <button onClick={eightBall}>Shake the ball!</button>
+          <button onClick={eightBallFunction}>Shake the ball!</button>
+          <h1>{eightBall}</h1>
 
         </div>
       </div>
