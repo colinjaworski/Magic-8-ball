9c70f4f14ccf60f3eb305124177d48844ec11162
colin jaworski <colinrjaworski.84@gmail.com>
2023-08-04 13:56:30 -0500

    made more changes, lol

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+
+
+
 
 function App() {
 
   const [eightBall, setEightBall] = useState('');
+  const [toggleBtn, setToggleBtn] = useState(true);
+
+  function toggleColor() {
+    setToggleBtn(!toggleBtn);
+    console.log('this is the btn state: ', toggleBtn)
+  }
 
   function eightBallFunction() {
 
     let randomNumber = Math.floor(Math.random() * 8);
-
 
     switch (randomNumber) {
       case 7: setEightBall('It is certain')
@@ -33,17 +41,16 @@ function App() {
   return (
     <>
       <header className="Header">
-        <h1>Magic 8 ball time!</h1>
+        <h1>Call me now, fer ya free trial reedin'</h1>
       </header>
       <div className="App">
         <div>
           <input type="checkbox" id="btnControl" />
           <label class="btn" for="btnControl">
-          <img onAnimationEnd={eightBallFunction} src="https://i5.walmartimages.com/asr/c3698a59-0624-4f8d-9e27-258139feb08c_1.9a6988dd9ed990c81dcab7afc9a37834.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
-            id="eightBall" height={400} width={400} />
-            </label>
-          <h1 id="response" >{eightBall}</h1>
-
+            <img onAnimationEnd={eightBallFunction} src="https://i5.walmartimages.com/asr/c3698a59-0624-4f8d-9e27-258139feb08c_1.9a6988dd9ed990c81dcab7afc9a37834.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
+              id="eightBall" height={400} width={400} />
+          </label>
+          <h1 id="response" onClick={toggleColor} style={{ background: toggleBtn ? "" : "red" }}>{eightBall}</h1>
         </div>
       </div>
     </>
